119978ee89022b2d14f5dc5cd4d4565ccbcb8f8a
Manoj Adhikari <53685400+jonamadk@users.noreply.github.com>
2020-08-12 13:31:52 +0545

    Update services.py

--- services.py
+++ services.py
@@ -9,4 +9,14 @@ def call_api(sleep_status):
     myobj = {"set_alarm":sleep_status}
 
     x = requests.patch(url, json= myobj)
+    
+    
+def call_api_mouth(mouth_status):
+
+    url = 'http://127.0.0.1:5000/post/1'
+    myobj1 = {"mouth_status":mouth_status}
+
+    x = requests.patch(url, json= myobj1)
+
+
   

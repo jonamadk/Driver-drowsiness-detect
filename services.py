6151518d0069d72bedfbcccd2756b0378deea13f
Manoj Adhikari <manojadhikari@sxc.edu.np>
2020-12-30 15:09:10 +0545

    added frame display

--- services.py
+++ services.py
@@ -9,4 +9,12 @@ def call_api(sleep_status):
     myobj = {"set_alarm":sleep_status}
 
     x = requests.patch(url, json= myobj)
+
+
+def call_api_mouth(mouth_status):
+
+    url = 'http://127.0.0.1:5000/post/1'
+    myboj ={"mouth_status":mouth_status}
+
+    x= requests.patch(url,json=myboj)
   
